8c9890ea3f9ab7c8e710f4c39b3e8d0df4038217
rocketII <vangandr@gmail.com>
2016-04-28 09:21:41 +0200

    starting solution.

---
+++ HashingEnglishWords.cpp
@@ -0,0 +1,19 @@
+//
+// Created by root on 2016-04-28.
+//
+#include "HashTable.h"
+int main(void)
+{
+    /*
+     * ska göras här.
+     * o Orden läses från fil
+     o Orden placeras i hashtabellen
+      o   En meny presenteras där användaren kan välja att
+     o mata in en mening för att få besked om vilka av orden i meningen som inte finns i
+       ordlistan (hashtabellen)
+     o lägg in nya ord
+     o ta bort ord
+     o avsluta
+     */
+    return 0;
+}
--- main.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by root on 2016-04-28.
-//
-#include "HashTable.h"
-int main(void)
-{
-    return 0;
-}
